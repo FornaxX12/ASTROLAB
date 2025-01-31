c1a6a08b1e68e6601ffed5eef83305318118e02c
FellipFB <felipeb.fnandes@gmail.com>
2025-01-31 20:55:17 -0300

    Atualizar o main.js
    Nova interface com dropdown(Teste)

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
 let searchBtn = document.querySelector(".bx-search");
+let dropdowns = document.querySelectorAll(".dropdown");
 
 closeBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("open");
-    menuBtnChange();
-})
+  sidebar.classList.toggle("open");
+  menuBtnChange();
+});
 
 searchBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("open");
-    menuBtnChange();
-})
+  sidebar.classList.toggle("open");
+  menuBtnChange();
+});
 
 function menuBtnChange() {
-    if (sidebar.classList.contains("open")) {
-        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
-    } else {
-        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
-    }
+  if (sidebar.classList.contains("open")) {
+    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+  } else {
+    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+  }
 }
+
+dropdowns.forEach(dropdown => {
+  let dropdownToggle = dropdown.querySelector('a');
+  let submenu = dropdown.querySelector('.submenu');
+  
+  dropdownToggle.addEventListener('click', (e) => {
+    e.preventDefault();
+    dropdown.classList.toggle('active'); 
+  });
+});
 
 menuBtnChange();
