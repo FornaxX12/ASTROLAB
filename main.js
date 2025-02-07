e456326f4cc5f68fcc2449b6b854b902ccbf4179
FellipFB <felipeb.fnandes@gmail.com>
2025-02-07 14:08:12 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
-let sidebar = document.querySelector(".sidebar");
-let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
-let dropdowns = document.querySelectorAll(".dropdown");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector("#btn");
+const searchBtn = document.querySelector(".bx-search");
+const dropdowns = document.querySelectorAll(".dropdown");
 
-closeBtn.addEventListener("click", () => {
+let startTouchX = 0;
+let endTouchX = 0;
+
+closeBtn.addEventListener("click", toggleSidebar);
+searchBtn.addEventListener("click", toggleSidebar);
+
+sidebar.addEventListener("touchstart", handleTouchStart);
+sidebar.addEventListener("touchend", handleTouchEnd);
+
+function toggleSidebar() {
   sidebar.classList.toggle("open");
   menuBtnChange();
   closeDropdowns();
-});
+}
 
-searchBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  menuBtnChange();
-  closeDropdowns();
-});
+function handleTouchStart(e) {
+  startTouchX = e.touches[0].clientX;
+}
+
+function handleTouchEnd(e) {
+  endTouchX = e.changedTouches[0].clientX;
+  const swipeDistance = Math.abs(startTouchX - endTouchX);
+
+  if (swipeDistance > 100) {
+    if (startTouchX > endTouchX && sidebar.classList.contains("open")) {
+      sidebar.classList.remove("open");
+      menuBtnChange();
+    } else if (endTouchX > startTouchX && !sidebar.classList.contains("open")) {
+      sidebar.classList.add("open");
+      menuBtnChange();
+    }
+  }
+}
 
 function menuBtnChange() {
   if (sidebar.classList.contains("open")) {
@@ -24,12 +46,10 @@ function menuBtnChange() {
 }
 
 dropdowns.forEach(dropdown => {
-  let dropdownToggle = dropdown.querySelector('a');
+  const dropdownToggle = dropdown.querySelector('a');
 
   dropdownToggle.addEventListener('click', (e) => {
-    if (!sidebar.classList.contains("open")) {
-      return;
-    }
+    if (!sidebar.classList.contains("open")) return;
 
     e.preventDefault();
     dropdown.classList.toggle('active');
